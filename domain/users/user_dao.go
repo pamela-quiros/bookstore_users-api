453c8a91f58fd9084269f673eaa5d4492c275f29
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-17 23:05:17 -0600

    Section 2: Creating Users API

---
+++ domain/users/user_dao.go
@@ -0,0 +1,41 @@
+package users
+
+import (
+	"fmt"
+
+	"github.com/pamela-quiros/bookstore_users-api/utils/date_utils"
+	"github.com/pamela-quiros/bookstore_users-api/utils/errors"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get() *errors.RestErr {
+	result := usersDB[user.Id]
+	if result == nil {
+		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+
+func (user *User) Save() *errors.RestErr {
+	current := usersDB[user.Id]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("user %d already exits", user.Id))
+	}
+	user.DateCreated = date_utils.GetNowString()
+
+	usersDB[user.Id] = user
+	return nil
+}
